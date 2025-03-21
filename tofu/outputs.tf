2028c256abb2f64827926bb06c07febfd3f80529
Pedro <pedro@yaroze.org>
2025-03-21 15:15:18 -0600

    Refactor outputs in Terraform configuration to remove unused variables and enhance schema documentation

--- tofu/outputs.tf
+++ tofu/outputs.tf
@@ -1,29 +1,9 @@
 output "nodesAvailabilityDomain" {
   value = var.node_availability_domain
 }
-
-output "workerCIDR" {
-  value = var.worker_subnet_cidr_block
-}
-
 output "k8sCompartmentID" {
   value = oci_identity_compartment.compartment.id
 }
-
-data "oci_containerengine_cluster_option" "node_pool_option" {
-  cluster_option_id = "all"
-
-}
-
-data "oci_containerengine_cluster_kube_config" "cluster_kube_config" {
-  cluster_id = oci_containerengine_cluster.my_cluster.id
-}
-
-
-output "kubeconfig" {
-  value = data.oci_containerengine_cluster_kube_config.cluster_kube_config.content
-}
-
 output "kubernetes_cluster_ocid" {
   value = oci_containerengine_cluster.my_cluster.id
 }
